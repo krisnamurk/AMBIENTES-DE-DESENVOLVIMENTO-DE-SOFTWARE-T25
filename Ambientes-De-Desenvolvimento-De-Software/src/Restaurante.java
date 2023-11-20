3ed5212f5d5b9bd414e493567d7dd54dbc0352f1
artursousac <artursousac@hotmail.com>
2023-11-19 21:58:22 -0300

    Criação da classe Restaurante e criação do construtor inicial.

---
+++ Ambientes-De-Desenvolvimento-De-Software/src/Restaurante.java
@@ -0,0 +1,13 @@
+public class Restaurante {
+    /*Esta classe é fazendo alusão ao Restaurante em si. Onde, a principio, teremos dois métodos que será um para fazer o pedido (para isso, será necessário
+     utilizarmos um método referente a outra classe, que será da classe Pedido, o método de adicionar item/pedido) neste restaurante e outro para listar os pedidos do Restaurante*/
+
+    public Restaurante(){
+        //Criação do construtor
+    }
+
+
+
+
+
+}
