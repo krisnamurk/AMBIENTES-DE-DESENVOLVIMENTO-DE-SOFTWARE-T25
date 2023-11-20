47fe1bf7306f94640e933ad40b9726631f2c42cb
artursousac <artursousac@hotmail.com>
2023-11-19 21:30:44 -0300

    Commit inicial, criando a classe Main e demais configurações

---
+++ Ambientes-De-Desenvolvimento-De-Software/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        /*Classe Main, onde será adicionada, posteriormente, todas as demais classes que irão se entregar em um exemplo relacionados ao sistema/gerenciamento
+        de pedidos de um Restaurante*/
+    }
+
+}
