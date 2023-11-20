0e2526b4763fea516c39a329f7976474e060fdf0
krisnamurkRolim <krisnamurk@hotmail.com>
2023-11-20 20:00:27 -0300

    add cliente

---
+++ Ambientes-De-Desenvolvimento-De-Software/src/Cliente.java
@@ -0,0 +1,10 @@
+public class Cliente {
+    private String nome;
+    private String endereco;
+
+    public Cliente(String nome, String endereco) {
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+}
+// aguardado alguns dados para iniciar
