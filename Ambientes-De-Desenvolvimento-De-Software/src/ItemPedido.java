7dfa8769cd5b81d6394aecf1ae3dc0021175f225
Célio Gonçalves <celiogoncalves40482@gmail.com>
2023-11-21 13:32:37 -0300

    nova classe ItemPedido

---
+++ Ambientes-De-Desenvolvimento-De-Software/src/ItemPedido.java
@@ -0,0 +1,11 @@
+public class ItemPedido {
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public ItemPedido(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+}
