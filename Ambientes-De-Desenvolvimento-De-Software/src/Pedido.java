45cd56d1bd4f7d572dbc2e103db4de9eb6b22948
Samuel Holanda <samuel_fdesigner@edu.unifor.br>
2023-11-24 18:56:55 -0300

    criação atributo ItemPedido + metodo adição pedido

--- Ambientes-De-Desenvolvimento-De-Software/src/Pedido.java
+++ Ambientes-De-Desenvolvimento-De-Software/src/Pedido.java
@@ -1,11 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pedido {
     private int numeroPedido;
     private Cliente cliente;
+    private List<ItemPedido> itens;
 
     public Pedido (int numeroPedido, Cliente cliente) {
         this.numeroPedido = numeroPedido;
         this.cliente = cliente;
-
+        this.itens = new ArrayList<>();
+    }
+    public void AdicionarItem(ItemPedido item){
+        itens.add(item);
+        System.out.println("Item adicionado ao pedido #" + numeroPedido);
     }
     
 }
